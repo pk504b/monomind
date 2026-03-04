3412a4cdfd80ea9b6dccd69559cf6c18f2f61f0d
Prince <pk.504b@gmail.com>
2026-03-04 21:39:08 +0100

    add icons

--- manifest.config.ts
+++ manifest.config.ts
@@ -8,7 +8,9 @@ export default defineManifest({
     "Transform your New Tab into a minimal, deep focus workspace designed to capture intent and archive your journey.",
   version: pkg.version,
   icons: {
+    16: "public/icon16.png",
     48: "public/icon48.png",
+    128: "public/icon128.png",
   },
   permissions: ["newTab"],
   chrome_url_overrides: {
